df0610f376def8ca3d1f3d345fe4c9c9d545dd4e
Triad01 <luckypee01@gmail.com>
2023-11-15 20:32:31 +0100

    added nop function

--- main.c
+++ main.c
@@ -30,6 +30,7 @@ int main(int argc, char *argv[])
 		{"pop", pop},
 		{"swap", swap},
 		{"add", add},
+		{"nop", nop},
 		{NULL, NULL}
 	};
 
--- op_funcs2.c
+++ op_funcs2.c
@@ -16,4 +16,15 @@ void add(stack_t **stack, unsigned int line_number)
 
 	pop(stack, line_number);
 }
+/**
+ * nop - doesn't do anything
+ * @stack: pointer to the stack
+ * @line_number: line number in the file
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	/* This function does nothing */
+}
 

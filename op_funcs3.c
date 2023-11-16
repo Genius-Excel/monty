72d0d6a30c642caaa7eed9540f41bb89d2acbf29
Genius-Excel <geniusexcel9@gmail.com>
2023-11-16 18:39:47 +0100

    I finally fixed the error.

--- op_funcs3.c
+++ op_funcs3.c
@@ -99,7 +99,7 @@ void mod(stack_t **stack, unsigned int line_number)
 
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL || (*(*stack)).next == NULL)
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);

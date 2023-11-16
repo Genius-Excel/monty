bdd0e7d14a3bfea395e66bda048afe172d28fddc
Triad01 <luckypee01@gmail.com>
2023-11-16 12:37:20 +0100

    corrected sub function

--- op_funcs3.c
+++ op_funcs3.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * sub - subtracts the top element of a stack from the second top
  * @stack: pointer to the stack
@@ -8,8 +9,7 @@ void sub(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L<line_number>: can't sub, stack too short");
-
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
@@ -17,3 +17,4 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	pop(stack, line_number); /* to pop the top element*/
 }
+
